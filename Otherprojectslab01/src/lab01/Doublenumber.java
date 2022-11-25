f1e0df97f999b6e0edef4cd4fa6d16f8e9712873
Nguyen Anh Duc <anhducntt2002@gmail.com>
2022-11-25 20:40:21 +0700

    update lab03

---
+++ Otherprojectslab01/src/lab01/Doublenumber.java
@@ -0,0 +1,21 @@
+package lab01;
+
+import java.util.Scanner;
+//import javax.swing.*;
+
+public class Doublenumber {
+    public static void main(String[] args){
+    	
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input 1st double: ");
+        double firstDouble = in.nextDouble();
+        System.out.print("Input 2nd double: ");
+        double secondDouble = in.nextDouble();
+
+        System.out.printf("Sum of two doubles: %.2f\n", firstDouble + secondDouble);
+        System.out.printf("Difference of two doubles: %.2f\n", firstDouble - secondDouble);
+        System.out.printf("Product of two doubles: %.2f\n", firstDouble * secondDouble);
+        System.out.printf("Quotient of two doubles: %.2f\n", (double) (firstDouble/ secondDouble));
+        
+    }
+}
---
+++ Otherprojectslab01/src/lab01/Helloworld.java
@@ -0,0 +1,9 @@
+package lab01;
+
+public class Helloworld {
+	public static void main(String[] args) {
+		System.out.println("Xin chao \ncac ban!");
+        System.out.println("Hello \t World");
+	}
+
+}
